2ce12fc41750e7ee7d6310b58f2f54c0333474de
aclinnovator <aclscientist@gmail.com>
2014-07-03 11:19:22 -0400

    First roboMEch commit

---
+++ RobotWar.spritebuilder/Source/Configuration.h
@@ -0,0 +1,18 @@
+//
+//  Configuration.h
+//  RobotWar
+//
+//  Created by Benjamin Encz on 05/06/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#ifndef RobotWar_Configuration_h
+#define RobotWar_Configuration_h
+
+static NSString *robotClass1 = @"AdvancedRobot";
+static NSString *robotCreator1 = @"Jeremy";
+
+static NSString *robotClass2 = @"SimpleRobot";
+static NSString *robotCreator2 = @"Benji";
+
+#endif
